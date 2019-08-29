6e5a6c00bffbf800933cc053dc2c5479afbd99d9
Monica <monicarigi@hotmail.com>
2019-08-29 15:44:01 +0700

    060402

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../styles/Thumbnail.css'
 
 class Thumbnail extends React.Component {
 	render() {
-		return (<div className="thumbnail"> 
+		return (<div className="thumbnail">
 		<h3>{this.props.title}</h3>
 		<small>{this.props.location}</small>
 		<h4>{this.props.price}</h4>
