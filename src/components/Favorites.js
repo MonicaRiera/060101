d63d4ec69cb61e862eef034f64727b3b51e73980
Monica <monicarigi@hotmail.com>
2019-08-29 14:49:58 +0700

    060302

---
+++ src/components/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Favorites.css'
+
+class Favorites extends React.Component {
+	state = {
+
+	}
+	render() {
+		return <div className="favorites"></div>
+	}
+}
+
+export default Favorites;
