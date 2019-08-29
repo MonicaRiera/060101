e8ad8b4571474638af169786989a033a32034388
Monica <monicarigi@hotmail.com>
2019-08-29 14:29:39 +0700

    060201

---
+++ src/components/Places.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Places extends React.Component {
+	state = {
+		places: [
+			{title:'Luxury Villa Indu Siam', price:350, location:'Koh Samui, Thailand'},
+			{title:'Villa Kelusa', price:400, location:'Bali, Indonesia'},
+			{title:'Tropical Architecture', price:100, location:'Koh Samui, Thailand'},
+			{title:'El Alma', price:1590, location:'Papagayo, Costa Rica'},
+			{title:'Villa Metisse', price:1230, location:'Tamarindo, Costa Rica'},
+			{title:'Laemsingh Villa', price:210, location:'Phuket, Thailand'},
+		]
+	}
+	render() {
+		return <div>Hello</div>
+	}
+}
