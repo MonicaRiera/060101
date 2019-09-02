a509f95f2eb4d4d899fff83568b29428b6f0eef8
Monica <monicarigi@hotmail.com>
2019-09-02 14:20:57 +0700

    060802

--- src/components/Place.js
+++ src/components/Place.js
@@ -9,22 +9,31 @@ class Place extends React.Component {
 			'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg',
 			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg',
 			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg'
-		]
+		],
+
+		backgroundImg : ''
 	}
 
-	// componentDidMount() {
-	// 	this.setState({
-	// 		backgroundImage: 'url(' + this.state.images[0] + ')'
-	// 	})
-	// }
+	changeImage = (index) => {
+		let img = this.state.backgroundImg
+		let images = this.state.images
+		img = images[index]
+		this.setState({backgroundImg: img})
+	}
+
+	componentWillMount() {
+		this.setState({backgroundImg: this.state.images[0]})
+	}
 
 	render () {
 		return (
 			<div className='main'>
-				<div className='bigImg' style={{backgroundImage : 'url(' + this.state.images[0] + ')'}}></div>
+				<div className='bigImg' style={{backgroundImage : 'url(' + this.state.backgroundImg + ')'}}></div>
 				<div className='gallery'>
 				{
-					this.state.images.map((e,i) => <div className='smallImg' style={{backgroundImage : 'url(' + this.state.images[i] + ')'}}></div>)
+					this.state.images.map((e,i) =>
+					<div className='smallImg' key={i} 
+					style={{backgroundImage : 'url(' + this.state.images[i] + ')'}} onClick={() => this.changeImage(i)}></div>)
 				}
 				</div>
 			</div>
