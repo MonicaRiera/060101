3807f1d54bfb6f2cdbc85eb590b1aa8a7365c54a
Monica <monicarigi@hotmail.com>
2019-09-02 12:08:35 +0700

    060801

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Places from './components/Places';
+import Routes from './components/Routes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Places />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
