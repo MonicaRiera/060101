e1a2ce411ad8e8b23288db48fb1021a14e32abf4
Monica <monicarigi@hotmail.com>
2019-09-02 15:03:57 +0700

    060803

--- src/components/Place.js
+++ src/components/Place.js
@@ -5,10 +5,10 @@ import '../styles/Place.css'
 class Place extends React.Component {
 	state = {
 		images : [
-			'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg',
-			'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg',
-			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg',
-			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg'
+			{image: 'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg', selected: true},
+			{image: 'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg', selected: false},
+			{image: 'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg', selected: false},
+			{image: 'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg', selected: false}
 		],
 
 		backgroundImg : ''
@@ -17,12 +17,15 @@ class Place extends React.Component {
 	changeImage = (index) => {
 		let img = this.state.backgroundImg
 		let images = this.state.images
-		img = images[index]
+		img = images[index].image
+		images.forEach((e, i) => {
+			i === index ? e.selected = true : e.selected = false
+		})
 		this.setState({backgroundImg: img})
 	}
 
 	componentWillMount() {
-		this.setState({backgroundImg: this.state.images[0]})
+		this.setState({backgroundImg: this.state.images[0].image})
 	}
 
 	render () {
@@ -32,8 +35,9 @@ class Place extends React.Component {
 				<div className='gallery'>
 				{
 					this.state.images.map((e,i) =>
-					<div className='smallImg' key={i} 
-					style={{backgroundImage : 'url(' + this.state.images[i] + ')'}} onClick={() => this.changeImage(i)}></div>)
+					<div className={this.state.images[i].selected ? 'smallImg selected' : 'smallImg'} key={i}
+					style={{backgroundImage : 'url(' + this.state.images[i].image + ')'}}
+					onClick={() => this.changeImage(i)}></div>)
 				}
 				</div>
 			</div>
